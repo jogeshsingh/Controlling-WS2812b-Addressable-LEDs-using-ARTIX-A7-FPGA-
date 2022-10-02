6462018d68eae002c0cbf52c124d426aa370bd75
Jogesh Singh <singhjogesh865@gmail.com>
2022-10-02 19:03:04 +0530

    Update TOP_DESIGN.v

--- RTL/TOP_DESIGN.v
+++ RTL/TOP_DESIGN.v
@@ -23,6 +23,7 @@
 module TOP_DESIGN(
 input i_clk , 
 input rst_n ,  
+    input [1:0] i_MUX, 
 output wire o_D, 
 output [2:0] p_STATE 
     );
@@ -33,7 +34,7 @@ output [2:0] p_STATE
   wire o_CNT_en ;
  
  
- /*   
+   
     MUX_4_1 MUX_UO2(
  .in_1(24'b00111100_01010101_10101010)  , 
  .in_2(24'b01110111_10111100_01110101) , 
@@ -42,7 +43,7 @@ output [2:0] p_STATE
  .i_sel(i_MUX), 
  .o_MUX(o_RGB) 
     );
-   */
+   
     
     LED_SWTICHING LED_U01(
  .i_clk(i_clk) , 
@@ -50,30 +51,12 @@ output [2:0] p_STATE
  .Red_in(o_RGB[15:8]) , 
  .Green_in(o_RGB[23:16]) , 
  .Blue_in(o_RGB[7:0]) , 
- .o_DOUT(o_D)  , 
- 
- .o_cnt_en(o_CNT_en),  
+ .o_DOUT(o_D)  ,  
  .p_STATE(p_STATE)  
  );
  
- assign o_RGB = o_CNT_en ? o_MEM_data : o_RGB ;
  
- 
- COUNTER U0_COUNTER(
-.i_clk(i_clk) ,  
-.i_rst_n(rst_n) , 
-.o_counter(o_COUNT)
-    );
-    
- 
-    
-   ROM U1_ROM_MEM(
-.i_addr_in(o_COUNT),
-.o_data(o_MEM_data)
- );
- 
- 
- 
+
    
  
  
